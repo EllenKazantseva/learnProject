6eee056d75e842969c4bc1e62027fd466c121da6
Ellen <helenkazanceva@gmail.com>
2023-11-10 18:35:07 +0300

    Objects about

--- jscript.js
+++ jscript.js
@@ -1,38 +1,42 @@
 "use strict";
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    }
+    makeTest: function() {
+        console.log("Test");
+    }
 };
 
-      for (let i = 0; i < 2; i++) {
-        const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', '');
+options.makeTest();
 
-      if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log('done');
-      } else {
-        console.log('error');
-        i--;
-      }
+const {border, bg} = options.colors;
 
-      if (personalMovieDB.count < 10) {
-        console.log("Просмотрено мало фильмов");
-      } else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
-        console.log("Вы хороший зритель");
-      } else if (personalMovieDB.count >= 30) {
-        console.log("Вы киноман");
-      } else {
-        console.log("Произошла ошибка");
-      }
-      
-      personalMovieDB.movies[a] = b;
-      }
+// console.log(Object.keys(options).length);
 
-      console.log(personalMovieDB);
+// console.log(options.["colors"]["border"]);
+
+// delete options.name;
+
+// console.log(options);
+
+// let counter = 0;
+
+// for (let key in options) {
+//     if (typeof(options[key]) === 'object') {
+//         for (let i in options[key]) {
+//             console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+//         }
+//     } else {
+//         console.log(`Свойство ${key} имеет значение ${options[key]}`);
+//         counter++;
+//     }
+// }
+
+// console.log(counter);
+
