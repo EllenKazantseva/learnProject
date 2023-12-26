f50caac3e3a48492a34522b878dba66945551ff0
Ellen <helenkazanceva@gmail.com>
2023-12-26 17:21:46 +0300

    main For transit

---
+++ forTasks.js
@@ -0,0 +1,11 @@
+const family = ['Peter', 'Ann', 'Alex', 'Linda'];
+
+function showFamily(arr) {
+    
+}
+
+const favoriteCities = ['liSBon', 'ROME', 'miLan', 'Dublin'];
+
+function standardizeStrings(arr) {
+    
+}
